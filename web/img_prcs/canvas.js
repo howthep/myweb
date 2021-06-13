fe63011b1fc44bc0ae3c814437eba0b68d824e56
chilleart <59827483+howthep@users.noreply.github.com>
2021-06-13 21:32:06 +0800

    Add files via upload

--- web/img_prcs/canvas.js
+++ web/img_prcs/canvas.js
@@ -11,23 +11,43 @@ context.fillRect(0, 0, 50, 50);
 
 
 function imgCreate(x,y,w,h){
-    var img = context.createImageData(w, h);
-
-        
-    imgPrcs(img)
+    var img = context.createImageData(w, h);   
+    gradient(img,155,200)
     context.putImageData(img, x, y);
 }
 
-function imgPrcs(img){
+function gradient(img,min,max){
+    var data = img.data;
     for (var i = 0; i < img.data.length; i += 4) {
-        var color = Math.floor(i/4/100)+150;  
-        img.data[i] = color*0.8;
-        img.data[i + 1] = color*0.1;
-        img.data[i + 2] = color+0.5;
-        img.data[i + 3] = 255;
+        var color=Math.floor(i/4/img.width)/img.height*(max-min)+min;
+        data[i]=color;
+        data[i+1]=color;
+        data[i+2]=color;
+        data[i+3]=255;
     }
-
 }
 
+var image = new Image();
+image.crossOrigin = "Anonymous";
+image.src='https://tse2-mm.cn.bing.net/th/id/OIP.Z_i-X1iOJpYTRDIKDp602gHaEK?w=289&h=180&c=7&o=5&pid=1.7'
 
-imgCreate(0,50,100,100);
+image.onload=function(){   
+    context.drawImage(image,100,100);
+    var idata=context.getImageData(100, 100, image.width, image.height);
+    decolor(idata.data);
+    context.putImageData(idata,100+image.width, 100);
+    
+}
+
+imgCreate(500,500,200,200)
+
+
+
+function decolor(data){
+    for(var i =0;i<data.length;i+=4){
+        var color = Math.round((data[i]+data[i+1]+data[i+2])/3)
+        data[i]=color;
+        data[i+1]=color;
+        data[i+2]=color;
+    }
+}
